d31fc9275b1138584b835e18a92753693bc61294
syarin-x <magicspell.max.mix.mutax@gmail.com>
2021-01-16 01:34:27 +0900

    test commit

--- mtcc.c
+++ mtcc.c
@@ -1,5 +1,12 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
 
 int main(int argc, char **argv){
     if(argc != 2){
